a48a6595a4f52bd5b8d5bfdba0f738156817f16e
Zack Davy <zacharyjdavy@users.noreply.github.com>
2020-04-29 18:07:57 +0000

    add routing

--- packages/zdmf-header-root/src/app/app-routing.module.ts
+++ packages/zdmf-header-root/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NullComponent } from './null.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '**', component: NullComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
