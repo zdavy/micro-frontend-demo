c2b7e6be2a77a8cb8af3a711da968c3ac234e35a
Zack Davy <zacharyjdavy@users.noreply.github.com>
2020-04-30 14:39:43 +0000

    fix names

--- packages/zdmf-demo-home/server.js
+++ packages/zdmf-demo-home/server.js
@@ -3,11 +3,11 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/consumer-one'));
+app.use(express.static(__dirname + '/dist/zdmf-demo-home'));
 
 app.get('/*', function(_, res) {
 
-  res.sendFile(path.join(__dirname + '/dist/consumer-one/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/zdmf-demo-home/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
